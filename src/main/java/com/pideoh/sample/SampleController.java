7f82ad8c324493a17a772ce6e7089b48cf927470
EC2 Default User <ec2-user@ip-172-31-6-153.ap-northeast-1.compute.internal>
2019-12-13 22:46:34 +0900

    First commit

---
+++ src/main/java/com/pideoh/sample/SampleController.java
@@ -0,0 +1,14 @@
+package com.pideoh.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class SampleController {
+
+    @GetMapping
+    public String sample() {
+        return "Sample\n";
+    }
+
+}
